20751b025635f94dba7e14fa22347f5633494fd5
tobiornot2b <tobias.maede@gmail.com>
2024-05-18 08:51:48 +0200

    install arandr

--- configuration.nix
+++ configuration.nix
@@ -107,6 +107,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    arandr
   ];
 
   environment.shells = with pkgs; [ zsh ];
