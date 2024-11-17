99552a3d3d75f14e37a2c7efcc14235a78928332
tobiornot2b <tobias.maede@gmail.com>
2024-11-17 17:50:47 +0100

    feat(sway): no titlebar, default resolution, keyboard shortcut for firefox

--- home/sway.nix
+++ home/sway.nix
@@ -1,9 +1,23 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 {
   wayland.windowManager.sway = {
     enable = true;
     config = rec {
       modifier = "Mod4";
+      window = {
+        border = 2;
+        titlebar = false;
+      };
+      output = {
+        Virtual-1 = {
+          mode = "2048x1152";
+        };
+      };
+      keybindings = let 
+          modifier = config.wayland.windowManager.sway.config.modifier;
+        in lib.mkOptionDefault {
+          "${modifier}+w" = "exec ${pkgs.firefox}/bin/firefox";
+        };
     };
   };
 }
