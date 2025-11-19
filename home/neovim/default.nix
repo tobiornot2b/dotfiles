d92bc5a561ea16ebc4b948f20ee96a677ba0a457
tobiornot2b <pgpg.toby@gmail.com>
2025-11-19 16:42:12 +0100

    feat(nvim): init nvim with symlink

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -6,4 +6,10 @@
     defaultEditor = true;
     vimAlias = true;
   };
+
+  # Plugins like treesitter need an C compiler
+  home.packages = lib.optionals config.programs.neovim.enable [
+    pkgs.gcc
+    pkgs.gnumake
+  ];
 }
--- hosts/dell-precision-5560/home.nix
+++ hosts/dell-precision-5560/home.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{pkgs, config, ...}: {
   imports = [
     ../../home
   ];
@@ -18,4 +18,8 @@
       };
     };
   };
+
+  # Create a symlink so the files can be changed without running home-manager again
+  # Path needs to be absolute to be working
+  home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/tobi/.dotfiles/config/nvim";
 }
