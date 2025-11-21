a2ab2ea4a0e6d7ef571adde8fa8f9ccffe9532bc
tobiornot2b <pgpg.toby@gmail.com>
2025-11-21 20:57:14 +0100

    feat(zsh): config p10k and autosuggestions

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ...}:
+{ config, pkgs, lib, ...}:
 
 {
   imports = [
@@ -15,9 +15,39 @@
 
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+
+    syntaxHighlighting = {
+      enable = true;
+      highlighters = [ "main" "brackets" "pattern" "regexp" "root" "line" ];
+    };
+    historySubstringSearch.enable = true;
+
+    history = {
+      ignoreDups = true;
+      save = 10000;
+      size = 10000;
+    };
+
     oh-my-zsh = {
       enable = true;
     };
+
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = lib.cleanSource ./p10k-config;
+        file = "p10k.zsh";
+      }
+    ];
+
     shellAliases = {
       ll = "ls -l";
       ".." = "cd ..";
