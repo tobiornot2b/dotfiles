91867dac494b29432a952ed98c829fae0c4ee74d
tobiornot2b <pgpg.toby@gmail.com>
2026-08-07 22:13:18 +0200

    chore(server): add standard cmdline tooling

--- hosts/contabo-server/home.nix
+++ hosts/contabo-server/home.nix
@@ -1,9 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ../../home
+  ];
   # Minimal home manager configuration for server
   # Root user has minimal customization
   home.stateVersion = "24.11";
   
-  # No shell customization needed; kept minimal
 }
